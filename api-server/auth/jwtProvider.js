e58080511939331b7040ad6298d39951d3191157
Shelly Gendler <shellygendler@gmail.com>
2024-03-03 01:16:04 +0200

    More indicative messages

--- api-server/auth/jwtProvider.js
+++ api-server/auth/jwtProvider.js
@@ -2,18 +2,16 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-
-
 const jwtProvider =  async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ error: "Authentication failed" });
+      return res.status(401).json({ error: "User does not exists" });
     }
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ error: "Authentication failed" });
+      return res.status(401).json({ error: "Inccorect password" });
     }
     const token = jwt.sign({ userId: user._id }, "your-secret-key", {
       expiresIn: "10h",
