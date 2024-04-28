c86f9dbd50a323dc8b10197425b31352e691b392
ShellyGendler <shellygendler10@gmail.com>
2024-04-28 12:05:45 +0300

    convert nodejs to client

--- api-server/index.js
+++ api-server/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const allRoutes = require("./routes");
+const tcpClient = require('./tcpClient');
+
 
 dotenv.config();
 
@@ -23,3 +25,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+setupTcpConnection();
